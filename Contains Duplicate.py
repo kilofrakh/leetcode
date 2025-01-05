771c20b23c6dd8f3fdfa956bd14d4a43f8596658
abdelkareem <81565331+kilofrakh@users.noreply.github.com>
2025-01-05 15:38:34 +0200

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,7 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] == nums[j]:
-                    return True
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+
         return False
